1dfb5e87f383724f4a3ae26f47c7a9faca8645a4
marudor <marudor@marudor.de>
2019-09-05 10:23:52 +0200

    Remove eslint comment

--- src/decorator.ts
+++ src/decorator.ts
@@ -3,7 +3,6 @@ import { createDecorator } from 'vue-class-component'
 // tslint:disable-next-line
 export const InjectModel = createDecorator((options: any, key) => {
   if (!options.injectModels) {
-    // eslint-disable-next-line no-param-reassign
     options.injectModels = []
   }
   options.injectModels.push(key)
